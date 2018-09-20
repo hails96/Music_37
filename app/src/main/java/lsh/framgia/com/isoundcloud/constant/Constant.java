6a6ef79a722c214645bf2accdfb0467159371d0d
Hai Luong <luongsonhai96@gmail.com>
2018-09-20 16:11:11 +0700

    Load more for genre screen

--- app/src/main/java/lsh/framgia/com/isoundcloud/constant/Constant.java
+++ app/src/main/java/lsh/framgia/com/isoundcloud/constant/Constant.java
@@ -8,8 +8,9 @@ public class Constant {
     public static final String KEY_AMBIENT = "ambient";
     public static final String KEY_CLASSICAL = "classical";
     public static final String KEY_COUNTRY = "country";
+    public static final String NEW_LINE = "\n";
     public static final String FORMAT_FETCHING_URL =
             "https://api-v2.soundcloud.com/charts?kind=top&genre=soundcloud%%3Agenres%%3A%s&" +
                     "&client_id=%s&offset=%d&limit=%d";
-    public static final String NEW_LINE = "\n";
+    public static final String FORMAT_DURATION = "%02d:%02d";
 }
--- app/src/main/java/lsh/framgia/com/isoundcloud/util/StringUtils.java
+++ app/src/main/java/lsh/framgia/com/isoundcloud/util/StringUtils.java
@@ -1,6 +1,7 @@
 package lsh.framgia.com.isoundcloud.util;
 
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import lsh.framgia.com.isoundcloud.BuildConfig;
 import lsh.framgia.com.isoundcloud.constant.Constant;
@@ -10,5 +11,14 @@ public class StringUtils {
     public static String formatFetchingTrackUrl(String genre, int offset, int limit) {
         return String.format(Locale.ENGLISH, Constant.FORMAT_FETCHING_URL,
                 genre, BuildConfig.ApiKey, offset, limit);
+    }
+
+    public static String convertMillisToDuration(int millis) {
+        return String.format(
+                Locale.ENGLISH,
+                Constant.FORMAT_DURATION,
+                TimeUnit.MILLISECONDS.toMinutes(millis),
+                TimeUnit.MILLISECONDS.toSeconds(millis) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
     }
 }
